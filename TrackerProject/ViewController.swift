95f602abd7ecb6e229a8a81fc7e91a58190701df
lilkxst <kastsianka.artsiom@gmail.com>
2023-10-24 04:09:24 +0300

    Initial Commit

---
+++ TrackerProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrackerProject
+//
+//  Created by Артём Костянко on 24.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
